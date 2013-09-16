20e9a3313c3373a6f9b48289cafb0a6367fb9ded
Tannin <devnull@localhost>
2013-09-16 22:32:42 +0200

    - bugfix: testing for missing masters at the wrong time seems to have caused crashes - bugfix: mod list is now written to a temporary file first. Only on success is the original file overwritten - bugfix: moving a mod priority to just above the overwrite could cause a crash or error message - bugfix: versions with a release candidate number weren't sorted correctly (woops) - bugfix: staging script didn't include archive.dll and dlls.manifest - installation time on overwrite no longer updates constantly

--- src/subrecord.cpp
+++ src/subrecord.cpp
@@ -18,9 +18,10 @@ void ESP::SubRecord::readFrom(std::istream &stream)
 {
   static std::unordered_map<std::string, EType> s_TypeMap = map_list_of("HEDR", TYPE_HEDR)("CNAM", TYPE_CNAM)("MAST", TYPE_MAST)("ONAM", TYPE_ONAM)("SNAME", TYPE_SNAM);
 
-  char typeString[4];
+  char typeString[5];
   stream.read(typeString, 4);
-  auto iter = s_TypeMap.find(std::string(typeString, 4));
+  typeString[4] = '\0'; // not sure if this is required, shouldn't be
+  auto iter = s_TypeMap.find(std::string(typeString));
   if (iter != s_TypeMap.end()) {
     m_Type = iter->second;
   } else {
@@ -28,6 +29,7 @@ void ESP::SubRecord::readFrom(std::istream &stream)
   }
 
   uint16_t dataSize = readType<uint16_t>(stream);
+
   m_Data.resize(dataSize);
   stream.read(reinterpret_cast<char*>(&m_Data[0]), dataSize);
 }
